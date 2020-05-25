7aa21b117f42d5ba9e4ff3bd25c1e312c5b1d817
wenhao <wenhao.z@live.com>
2020-05-24 18:31:56 -0700

    Fixed some formatting for message related files

--- src/repositories/message.js
+++ src/repositories/message.js
@@ -1,67 +1,64 @@
 const errors = require("../utils/error/errors");
 module.exports = [
-    "msgModel",
-    (msgModel) => {
-//todo: catching errors
-        const get = async (opt) => {
-            const message = await msgModel.findOne(opt.query);
+  "msgModel",
+  (msgModel) => {
+    //todo: catching errors
+    const get = async (opt) => {
+      const message = await msgModel.findOne(opt.query);
 
-            if (!message) {
-                throw new Error(errors.MESSAGE_NOT_EXIST);
-              }
+      if (!message) {
+        throw new Error(errors.MESSAGE_NOT_EXIST);
+      }
 
-            return message;
+      return message;
+    };
 
-        }
+    return {
+      create: async (opt) => {
+        const newMessage = new msgModel(opt.query);
+        await newMessage.save();
 
-        return{
-            create: async (opt) => {
-                const newMessage = new msgModel(opt.query);
-                await newMessage.save();
+        return newMessage;
+      },
 
-                return newMessage;
-            },
+      createMany: async (opt) => {
+        const array = opt.queries;
 
-            createMany:async(opt) => {
+        const messages = await msgModel.insertMany(array);
 
-                const array = opt.queries;
+        return messages;
+      },
 
-                const messages = await msgModel.insertMany(array);
+      get,
 
-                return messages;
-            },
+      getMessageForRoom: async (opt) => {
+        const messages = await msgModel.find(opt.query); //todo: sort and return limit
 
-            get,
+        const messageBunch = [];
 
-            getMessageForRoom: async (opt) => {
-                const messages = await msgModel.find(opt.query);//todo: sort and return limit
+        messages.forEach((message) => {
+          messageBunch.push(message);
+        });
 
-                const messageBunch = [];
+        return messageBunch;
+      },
 
-                messages.forEach ((message) => {
-                    messageBunch.push(message);
-                })
+      delete: async (opt) => {
+        const message = await get(opt);
+        await message.remove();
+        return message;
+      },
 
-                return messageBunch;
-            },
+      update: async (opt) => {
+        const message = await get(opt);
+        const updates = Object.keys(opt.update);
 
-            delete: async(opt) => {
-                const message = await get(opt);
-                await message.remove();
-                return message;
-            },
+        message[updates] = opt.update[updates];
 
-            update: async(opt) =>{
-                const message = await get(opt);
-                const updates = Object.keys(opt.update);
+        message.save();
 
-                message[updates] = opt.update[updates];
-
-                message.save();
-
-                return message;
-
-            }
-        };
-    },
+        return message;
+      },
+    };
+  },
 ];
