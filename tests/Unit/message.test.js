b1e7496fd0622b19ea8b278543c63eb742497630
Vincent <wenweil@sfu.ca>
2020-05-27 14:59:06 -0700

    grouped tests using describe

--- tests/Unit/message.test.js
+++ tests/Unit/message.test.js
@@ -24,255 +24,267 @@ const repo = {
 const functions = controller[1](repo);
 var retval;
 
-test("new message normal message", async () => {
-  const time = new Date(1590021724501);
-  const body = {
-    text: "aaaaa",
-    author: "5dd4dcfe36424d441068f7aa",
-    room: "5dd4dcfe36424d441068f7aa",
-    isPinned: false,
-    time,
-    //time: 1590021724501
-  };
-  const req = {
-    body,
-  };
+describe("new message tests", () => {
+  test("new message normal message", async () => {
+    const time = new Date(1590021724501);
+    const body = {
+      text: "aaaaa",
+      author: "5dd4dcfe36424d441068f7aa",
+      room: "5dd4dcfe36424d441068f7aa",
+      isPinned: false,
+      time,
+      //time: 1590021724501
+    };
+    const req = {
+      body,
+    };
 
-  const res = {
-    json: (ret) => {
-      expect(ret).toEqual(body);
-    },
-  };
-  const next = {};
-  await functions.newMessage(req, res, next);
+    const res = {
+      json: (ret) => {
+        expect(ret).toEqual(body);
+      },
+    };
+    const next = {};
+    await functions.newMessage(req, res, next);
+  });
+
+  test("new message not allowed", async () => {
+    var hasError = false;
+    const time = new Date(1590021724501);
+    const body = {
+      text: "",
+      author: "5dd4dcfe36424d441068f6aa",
+      room: "5dd4dcfe36424d441068f7aa",
+      isPinned: false,
+      time,
+      //time: 1590021724501
+    };
+    const req = {
+      body,
+    };
+    const res = {
+      json: (ret) => {},
+    };
+    const next = (error) => {
+      hasError = true;
+      const err = new Error(errors.PLEASE_AUTHENTICATE);
+      err.name = errors.PLEASE_AUTHENTICATE;
+      expect(error).toEqual(err);
+    };
+    await functions.newMessage(req, res, next);
+    expect(hasError).toBe(true);
+  });
+
+  test("new message no text", async () => {
+    var hasError = false;
+    const time = new Date(1590021724501);
+    const body = {
+      text: "",
+      author: "5dd4dcfe36424d441068f7aa",
+      room: "5dd4dcfe36424d441068f7aa",
+      isPinned: false,
+      time,
+      //time: 1590021724501
+    };
+    const req = {
+      body,
+    };
+
+    const res = {
+      json: (ret) => {},
+    };
+    const next = (error) => {
+      hasError = true;
+      const err = new Error(errors.MESSAGE_NO_TEXT);
+      err.name = errors.MESSAGE_NO_TEXT;
+      expect(error).toEqual(err);
+    };
+    await functions.newMessage(req, res, next);
+    expect(hasError).toBe(true);
+  });
 });
 
-test("normal delete message", async () => {
-  const body = {
-    ID: "5dd4dcfe36424d441068f7aa",
-  };
-  const req = {
-    body,
-  };
-  const res = {
-    json: (ret) => {
-      expect(ret._id).toEqual(body.ID);
-    },
-  };
-  const next = {};
-  await functions.deleteMessage(req, res, next);
+describe("delete message tests", () => {
+  test("normal delete message", async () => {
+    const body = {
+      ID: "5dd4dcfe36424d441068f7aa",
+    };
+    const req = {
+      body,
+    };
+    const res = {
+      json: (ret) => {
+        expect(ret._id).toEqual(body.ID);
+      },
+    };
+    const next = {};
+    await functions.deleteMessage(req, res, next);
+  });
+
+  test("delete message not found", async () => {
+    var hasError = false;
+    const body = {
+      ID: "5dd4dcfe36424d441068faa",
+    };
+    const req = {
+      body,
+    };
+    const res = {
+      json: (ret) => {},
+    };
+    const next = (error) => {
+      hasError = true;
+      const err = new Error(errors.MESSAGE_NOT_EXIST);
+      err.name = errors.MESSAGE_NOT_EXIST;
+      expect(error).toEqual(err);
+    };
+    await functions.deleteMessage(req, res, next);
+    expect(hasError).toBe(true);
+  });
+
+  test("delete message not allowed", async () => {
+    var hasError = false;
+    const body = {
+      ID: "5dd4dcfe36424d441068faa",
+    };
+    const req = {
+      body,
+    };
+    const res = {
+      json: (ret) => {},
+    };
+    const next = (error) => {
+      hasError = true;
+      const err = new Error(errors.PLEASE_AUTHENTICATE);
+      err.name = errors.PLEASE_AUTHENTICATE;
+      expect(error).toEqual(err);
+    };
+    await functions.deleteMessage(req, res, next);
+    expect(hasError).toBe(true);
+  });
 });
 
-test("normal edit message", async () => {
-  const body = {
-    ID: "5dd4dcfe36424d441068f7aa",
-    text: "sdfw",
-  };
-  const req = {
-    body,
-  };
+describe("edit message tests", () => {
+  test("normal edit message", async () => {
+    const body = {
+      ID: "5dd4dcfe36424d441068f7aa",
+      text: "sdfw",
+    };
+    const req = {
+      body,
+    };
 
-  const res = {
-    json: (ret) => {
-      expect(ret.query._id).toEqual(body.ID);
-      expect(ret.update.text).toEqual(body.text);
-    },
-  };
-  const next = {};
-  await functions.editMessage(req, res, next);
-});
+    const res = {
+      json: (ret) => {
+        expect(ret.query._id).toEqual(body.ID);
+        expect(ret.update.text).toEqual(body.text);
+      },
+    };
+    const next = {};
+    await functions.editMessage(req, res, next);
+  });
 
-test("normal get message", async () => {
-  const body = {
-    ID: "5dd4dcfe36424d441068f7aa",
-  };
-  const req = {
-    body,
-  };
-  const res = {
-    json: (ret) => {
-      expect(ret._id).toEqual(body.ID);
-    },
-  };
-  const next = (error) => {};
-  await functions.getMessage(req, res, next);
-});
+  test("edit message not found", async () => {
+    var hasError = false;
+    const body = {
+      ID: "5dd4dcfe36424d441068faa",
+    };
+    const req = {
+      body,
+    };
+    const res = {
+      json: (ret) => {},
+    };
+    const next = (error) => {
+      hasError = true;
+      const err = new Error(errors.MESSAGE_NOT_EXIST);
+      err.name = errors.MESSAGE_NOT_EXIST;
+      expect(error).toEqual(err);
+    };
+    await functions.editMessage(req, res, next);
+    expect(hasError).toBe(true);
+  });
 
-test("new message not allowed", async () => {
-  var hasError = false;
-  const time = new Date(1590021724501);
-  const body = {
-    text: "",
-    author: "5dd4dcfe36424d441068f6aa",
-    room: "5dd4dcfe36424d441068f7aa",
-    isPinned: false,
-    time,
-    //time: 1590021724501
-  };
-  const req = {
-    body,
-  };
-  const res = {
-    json: (ret) => {},
-  };
-  const next = (error) => {
-    hasError = true;
-    const err = new Error(errors.PLEASE_AUTHENTICATE);
-    err.name = errors.PLEASE_AUTHENTICATE;
-    expect(error).toEqual(err);
-  };
-  await functions.newMessage(req, res, next);
-  expect(hasError).toBe(true);
-});
-test("new message no text", async () => {
-  var hasError = false;
-  const time = new Date(1590021724501);
-  const body = {
-    text: "",
-    author: "5dd4dcfe36424d441068f7aa",
-    room: "5dd4dcfe36424d441068f7aa",
-    isPinned: false,
-    time,
-    //time: 1590021724501
-  };
-  const req = {
-    body,
-  };
+  test("edit message no text", async () => {
+    var hasError = false;
+    const body = {
+      ID: "5dd4dcfe36424d441068f7aa",
+      text: "",
+    };
+    const req = {
+      body,
+    };
 
-  const res = {
-    json: (ret) => {},
-  };
-  const next = (error) => {
-    hasError = true;
-    const err = new Error(errors.MESSAGE_NO_TEXT);
-    err.name = errors.MESSAGE_NO_TEXT;
-    expect(error).toEqual(err);
-  };
-  await functions.newMessage(req, res, next);
-  expect(hasError).toBe(true);
+    const res = {
+      json: (ret) => {},
+    };
+    const next = (error) => {
+      hasError = true;
+      const err = new Error(errors.MESSAGE_NO_TEXT);
+      err.name = errors.MESSAGE_NO_TEXT;
+      expect(error).toEqual(err);
+    };
+    await functions.editMessage(req, res, next);
+    expect(hasError).toBe(true);
+  });
+
+  test("edit message not allowed", async () => {
+    var hasError = false;
+    const body = {
+      ID: "5dd4dcfe36424d441068faa",
+    };
+    const req = {
+      body,
+    };
+    const res = {
+      json: (ret) => {},
+    };
+    const next = (error) => {
+      hasError = true;
+      const err = new Error(errors.PLEASE_AUTHENTICATE);
+      err.name = errors.PLEASE_AUTHENTICATE;
+      expect(error).toEqual(err);
+    };
+    await functions.editMessage(req, res, next);
+    expect(hasError).toBe(true);
+  });
 });
 
-test("delete message not found", async () => {
-  var hasError = false;
-  const body = {
-    ID: "5dd4dcfe36424d441068faa",
-  };
-  const req = {
-    body,
-  };
-  const res = {
-    json: (ret) => {},
-  };
-  const next = (error) => {
-    hasError = true;
-    const err = new Error(errors.MESSAGE_NOT_EXIST);
-    err.name = errors.MESSAGE_NOT_EXIST;
-    expect(error).toEqual(err);
-  };
-  await functions.deleteMessage(req, res, next);
-  expect(hasError).toBe(true);
-});
-test("delete message not allowed", async () => {
-  var hasError = false;
-  const body = {
-    ID: "5dd4dcfe36424d441068faa",
-  };
-  const req = {
-    body,
-  };
-  const res = {
-    json: (ret) => {},
-  };
-  const next = (error) => {
-    hasError = true;
-    const err = new Error(errors.PLEASE_AUTHENTICATE);
-    err.name = errors.PLEASE_AUTHENTICATE;
-    expect(error).toEqual(err);
-  };
-  await functions.deleteMessage(req, res, next);
-  expect(hasError).toBe(true);
-});
+describe("get message tests", () => {
+  test("normal get message", async () => {
+    const body = {
+      ID: "5dd4dcfe36424d441068f7aa",
+    };
+    const req = {
+      body,
+    };
+    const res = {
+      json: (ret) => {
+        expect(ret._id).toEqual(body.ID);
+      },
+    };
+    const next = (error) => {};
+    await functions.getMessage(req, res, next);
+  });
 
-test("edit message not found", async () => {
-  var hasError = false;
-  const body = {
-    ID: "5dd4dcfe36424d441068faa",
-  };
-  const req = {
-    body,
-  };
-  const res = {
-    json: (ret) => {},
-  };
-  const next = (error) => {
-    hasError = true;
-    const err = new Error(errors.MESSAGE_NOT_EXIST);
-    err.name = errors.MESSAGE_NOT_EXIST;
-    expect(error).toEqual(err);
-  };
-  await functions.editMessage(req, res, next);
-  expect(hasError).toBe(true);
-});
-test("edit message no text", async () => {
-  var hasError = false;
-  const body = {
-    ID: "5dd4dcfe36424d441068f7aa",
-    text: "",
-  };
-  const req = {
-    body,
-  };
-
-  const res = {
-    json: (ret) => {},
-  };
-  const next = (error) => {
-    hasError = true;
-    const err = new Error(errors.MESSAGE_NO_TEXT);
-    err.name = errors.MESSAGE_NO_TEXT;
-    expect(error).toEqual(err);
-  };
-  await functions.editMessage(req, res, next);
-  expect(hasError).toBe(true);
-});
-test("edit message not allowed", async () => {
-  var hasError = false;
-  const body = {
-    ID: "5dd4dcfe36424d441068faa",
-  };
-  const req = {
-    body,
-  };
-  const res = {
-    json: (ret) => {},
-  };
-  const next = (error) => {
-    hasError = true;
-    const err = new Error(errors.PLEASE_AUTHENTICATE);
-    err.name = errors.PLEASE_AUTHENTICATE;
-    expect(error).toEqual(err);
-  };
-  await functions.editMessage(req, res, next);
-  expect(hasError).toBe(true);
-});
-
-test("get message not found", async () => {
-  var hasError = false;
-  const body = {
-    ID: "5dd4dcfe36424d441068faa",
-  };
-  const req = {
-    body,
-  };
-  const res = {
-    json: (ret) => {},
-  };
-  const next = (error) => {
-    hasError = true;
-    const err = new Error(errors.MESSAGE_NOT_EXIST);
-    err.name = errors.MESSAGE_NOT_EXIST;
-    expect(error).toEqual(err);
-  };
-  await functions.getMessage(req, res, next);
-  expect(hasError).toBe(true);
+  test("get message not found", async () => {
+    var hasError = false;
+    const body = {
+      ID: "5dd4dcfe36424d441068faa",
+    };
+    const req = {
+      body,
+    };
+    const res = {
+      json: (ret) => {},
+    };
+    const next = (error) => {
+      hasError = true;
+      const err = new Error(errors.MESSAGE_NOT_EXIST);
+      err.name = errors.MESSAGE_NOT_EXIST;
+      expect(error).toEqual(err);
+    };
+    await functions.getMessage(req, res, next);
+    expect(hasError).toBe(true);
+  });
 });
