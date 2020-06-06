646c231a786e07b2e691d02b8f3e0b55edce2a81
Vincent <wenweil@sfu.ca>
2020-06-05 19:26:24 -0700

    even more fixes

--- src/utils/error/handler.js
+++ src/utils/error/handler.js
@@ -2,24 +2,24 @@ const errors = require("./errors");
 const httpStatus = require("../httpStatus");
 
 module.exports = (err, req, res, next) => {
-
   let statusMessage;
   let status;
   let message;
-  let retval =   { status, message };
-
+  let retval = { status, message };
 
   if (err.name === "MongoError") {
-      retval.status = httpStatus.BAD_REQUEST;
-      retval.message = err.message;
-      statusMessage = httpStatus.getStatusText(httpStatus.BAD_REQUEST);
+    retval.status = httpStatus.BAD_REQUEST;
+    retval.message = err.message;
+    statusMessage = httpStatus.getStatusText(httpStatus.BAD_REQUEST);
   } else {
-    retval = errors.getErrorInfo(err.name || err.message);
-    statusMessage = httpStatus.getStatusText(retval.status);
+    const { status, message } = errors.getErrorInfo(err.name || err.message);
+    statusMessage = httpStatus.getStatusText(status);
+    retval.status = status;
+    retval.message = message;
   }
   res.status(retval.status).send({
     error: retval.message,
     message: statusMessage,
-    status: retval.status
+    status: retval.status,
   });
 };
