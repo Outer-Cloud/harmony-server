64fb0ca55dfc7861f8f4e4c2f052d06903508197
Vincent <wenweil@sfu.ca>
2020-06-17 19:32:53 -0700

    edit route as per review

--- src/routes/message.js
+++ src/routes/message.js
@@ -7,9 +7,9 @@ module.exports = [
     const router = new express.Router();
 
     router.post("/", auth,  msgController.newMessage);
-    router.get("/get/:id", auth, msgController.getMessage);
-    router.delete("/delete/:id", auth, msgController.deleteMessage);
-    router.patch("/edit/:id", auth, msgController.editMessage);
+    router.get("/:id", auth, msgController.getMessage);
+    router.delete("/:id", auth, msgController.deleteMessage);
+    router.patch("/:id", auth, msgController.editMessage);
     router.get("/getRoom/:room", auth, msgController.getRoom);
 
     return router;
