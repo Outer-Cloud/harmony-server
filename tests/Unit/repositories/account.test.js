b504c0c2051ceead02f199c11599aa03d3ef4a92
wenhao <wenhao.z@live.com>
2020-06-17 21:00:17 -0700

    Update errors and valiation and make some modules injected
    -bcrypt and jsonwebtoken will now be injected (Makes mocking these in a test easier)
    -rename validation to utils
    -move token generation into util file
    -all modules under /utils will be injected
    -update test accordingly
    -update controller accordingly

---
+++ tests/Unit/repositories/account.test.js
@@ -0,0 +1,13 @@
+const account = require("../../../src/repositories/account");
+const getRepository = account[account.length - 1];
+
+describe("Account repository tests", () => {
+  describe("create", () => {});
+  describe("get", () => {});
+  describe("update", () => {});
+  describe("delete", () => {});
+  describe("findByCredentials", () => {});
+  describe("getUserId", () => {});
+  describe("generateAuthToken", () => {});
+  describe("deleteTokens", () => {});
+});
