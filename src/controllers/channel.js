45abed73646ff97bfa8c5df5521730d465936a3c
Vincent <wenweil@sfu.ca>
2020-06-22 23:09:22 -0700

    init channel files,

---
+++ src/controllers/channel.js
@@ -0,0 +1,11 @@
+module.exports = [
+    "msgRepository",
+    "channelRepository",
+    "utils",
+    "errors",
+    (msgRepository,channelRepository, utils, errors) => {
+      const errorCodes = errors.errorCodes;
+      const { isValid, invalid } = utils;
+      //todo: finish this
+    }
+]
